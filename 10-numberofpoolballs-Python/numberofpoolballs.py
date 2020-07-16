92ce66c85f709a671fc114f71ece7f6d50e222dc
priyanka24798 <kotupriyanka@msitprogram.net>
2020-07-16 13:29:03 +0530

    Jul 16  13:29:03

--- 10-numberofpoolballs-Python/numberofpoolballs.py
+++ 10-numberofpoolballs-Python/numberofpoolballs.py
@@ -8,5 +8,16 @@
 # number of rows. For this problem and the next, you should research Triangular Numbers.
 
 def numberofpoolballs(rows):
-	# Your code goes here
-	pass
+	# Your code goes here\\
+	temp = rows
+	c = 0
+	while(temp > 0):
+		c = c + temp
+		temp = temp +1
+	return c
+
+
+
+
+
+
