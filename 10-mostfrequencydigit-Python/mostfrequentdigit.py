dccad742d44bda015e8b926051f5271c26571afa
priyanka24798 <kotupriyanka@msitprogram.net>
2020-07-17 00:22:45 +0530

    frequent

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -3,5 +3,22 @@
 # that occurs most frequently in it, with ties going to the smaller digit.
 
 def mostfrequentdigit(n):
-	# your code goes here
-	pass
+	if n == 0:
+		return 0
+	if n > 0:
+		a = str(n)
+		lst =[]
+		count = 0
+		for i in a:
+			lst.append(i)
+			s = list(map(int,lst))
+		max = 0
+		res = s[0] 
+		for i in s: 
+			freq = s.count(i)
+			if freq > max: 
+				max = freq 
+				res = i 
+		
+		return res
+mostfrequentdigit(2342)
