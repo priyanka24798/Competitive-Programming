f3eca90d8b1615b717104503acdb6f1994160005
priyanka24798 <kotupriyanka@msitprogram.net>
2020-07-17 13:06:44 +0530

    Jul 17  13:06:44

--- 05-interleave-Python/interleave.py
+++ 05-interleave-Python/interleave.py
@@ -33,11 +33,11 @@ def fun_interleave(s1,s2):
 		result = ""
 		for i in range(0,nu):
 			result += s1[i] + s2[i]
-		if len(s1) < len(s2):
-			result += s2[nu:len(s2)]
-		else:
-			result += s1[nu:len(s1)]
+			if len(s1) < len(s2):
+				result += s2[nu:len(s2)]
+			else:
+				result += s1[nu:len(s1)]
 
 
 	# print(sr)
-	return sr
+	return result
