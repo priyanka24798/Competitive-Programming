f7a702b2adda2a118b39a74c587b412ce3fd529a
priyanka24798 <kotupriyanka@msitprogram.net>
2020-07-20 19:56:52 +0530

    Jul 20  19:56:51

--- 07-isfactorish-Python/isfactorish.py
+++ 07-isfactorish-Python/isfactorish.py
@@ -12,27 +12,30 @@
 
 
 def fun_isfactorish(n):
-    a = str(n)
-    # n = abs(n)
-    if len(str((abs(n))))!=3:
-        return False
-    else:
-        lst = []
-        n = str(abs(n))
-        for i in n:
-            lst.append(i)
-            int_lst = list(map(int,lst))
-        # if len(set(int_lst)) == len(int_lst):
-        #     return False
-    
-        for i in int_lst:
-            print(i)
-            if int(n) % i != 0:
-                return False
-            if i == 0:
-                return False
-        return True
-fun_isfactorish(412)
+	a = str(n)
+	# n = abs(n)
+	if len(str((abs(n))))!=3:
+		return False
+	else:
+		lst = []
+		n = str(abs(n))
+		for i in n:
+			lst.append(i)
+			int_lst = list(map(int,lst))
+		# if len(set(int_lst)) == len(int_lst):
+		#     return False
+
+		for i in int_lst:
+			print(i)
+			if i == 0:
+				return False
+			# if i == 0:
+			# 	return False
+			elif int(n) % i != 0:
+				return False
+			
+		return True
+	fun_isfactorish(412)
 	
 
 # 	# return False
