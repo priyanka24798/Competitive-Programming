1a27c17a282edeceec1a82a7f7d2503a22913d8b
priyanka24798 <kotupriyanka@msitprogram.net>
2020-07-21 17:18:54 +0530

    inverse

--- 07-inverselookandsay-Python/inverselookandsay.py
+++ 07-inverselookandsay-Python/inverselookandsay.py
@@ -9,5 +9,22 @@
 # inverseLookAndSay([(2,3),(1,8),(4,3)]) == [3,3,8,3,3,3,3])
 
 def inverselookandsay(a):
+	
+	lst = []
+	if a == [()]:
+		return []
+	else:
+		for i in a:
+			print(i)
+			count = i[0]
+			num = i[1]
+			result = [num]*count
+			lst = lst + result
+	print(count)	
+	print(num)
+	return lst
+
+	
+
+inverselookandsay([(2,3),(1,8),(3,-10)])
 	# Your code goes here
-	pass
